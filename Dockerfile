4d5201936611a40344b2dbe87721174e4c5e4875
Ivan <support@wintuning.ru>
2020-11-17 23:14:07 +0300

    Added Classes for DB, Addresses. Fixed deleting and updating methods. Fixed docker file and added .sql file with db sctructure

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,8 @@ FROM python:3.9
 
 WORKDIR /home
 
-ENV TELEGRAM_API_TOKEN=""
-ENV YANDEX_API_KEY =""
+#ENV TELEGRAM_API_TOKEN=""
+#ENV YANDEX_API_KEY =""
 ENV TELEGRAM_ACCESS_ID=""
 ENV TELEGRAM_PROXY_URL=""
 ENV TELEGRAM_PROXY_LOGIN=""
@@ -15,5 +15,9 @@ RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 COPY requirements.txt ./
 RUN pip install -r requirements.txt && apt-get update && apt-get install sqlite3
 COPY *.py ./
+COPY *.sql ./
+RUN mkdir db
+COPY db/* ./db/
+RUN cat ./createdb.sql | sqlite3  ./db/addresses.db
 
 ENTRYPOINT ["python3", "server.py"]
---
+++ createdb.sql
@@ -0,0 +1,32 @@
+drop table if exists addresses;
+drop table if exists categories;
+
+create table addresses(
+    id integer primary key,
+    user_id integer,
+    address varchar(500),
+    link_to_ya_map text,
+    created datetime,
+    category_id integer,
+    FOREIGN KEY(category_id) REFERENCES categories(id)
+);
+
+create table categories(
+    id integer primary key,
+    user_id integer,
+    name varchar(255),
+    created datetime
+);
+
+insert into categories (id, name, user_id)
+values
+    (1, "Рабочий", 266431296),
+    (2, "Домашний", 266431296),
+    (3, "Другой", 266431296);
+
+insert into addresses (id, address, link_to_ya_map, created, user_id, category_id)
+values
+    (1, "Домашний адрес 1", "link1", date('now', 'localtime'), 266431296, 2),
+    (2, "Рабочий адрес 1", "link2", date('now', 'localtime'), 266431296, 1),
+    (3, "Домашний адрес 2", "link3", date('now', 'localtime'), 266431296, 2),
+    (4, "Домашний адрес 3", "link4", date('now', 'localtime'), 266431296, 2);
