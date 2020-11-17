57b8bfe7a0083c51335e271c17aef9329b69b9da
Ivan <support@wintuning.ru>
2020-11-17 13:31:25 +0300

    Saving and deleting addresses to/from Database added Added addresses.py script Added db.py script

---
+++ addresses.py
@@ -0,0 +1,67 @@
+import datetime
+import re
+from typing import List, NamedTuple, Optional
+
+import pytz
+
+import db
+import exceptions
+
+
+class Address(NamedTuple):
+    """Структура добавленного в БД адреса"""
+    id: Optional[int]
+    address: str
+    link_to_ya_map: str
+    user_id: int
+
+
+def add_address(address: str, link_to_ya_map: str, user_id: int) -> str:
+    """Добавляет новый адрес в базу данных.
+    Принимает на вход текст сообщения, поступившего на вход в бот"""
+    """
+    inserted_user_id = db.insert('users',
+                    {
+                        "id": user_id,
+                        "created": _get_now_formatted(),
+                        "last_updated": _get_now_formatted(),
+                    }
+               )
+    """
+
+    inserted_id = db.insert('addresses',
+                    {
+                        "address": address,
+                        "link_to_ya_map": link_to_ya_map,
+                        "created": _get_now_formatted(),
+                        "user_id": user_id
+                    }
+               )
+    return inserted_id
+
+
+def _get_now_formatted() -> str:
+    """Возвращает сегодняшнюю дату строкой"""
+    return _get_now_datetime().strftime("%Y-%m-%d %H:%M:%S")
+
+
+def _get_now_datetime() -> datetime.datetime:
+    """Возвращает сегодняшний datetime с учётом времненной зоны Мск."""
+    tz = pytz.timezone("Europe/Moscow")
+    now = datetime.datetime.now(tz)
+    return now
+
+
+def get_all_addresses(user_id) -> List[Address]:
+    """Возвращает список всех адресов, введенных пользователем"""
+    cursor = db.get_cursor()
+    cursor.execute("select id, address, link_to_ya_map, user_id "
+                   f"from addresses where user_id = {user_id} "
+                   "order by created desc LIMIT 100")
+    rows = cursor.fetchall()
+    addresses = [Address(id=row[0], address=row[1], link_to_ya_map=row[2], user_id=row[3]) for row in rows]
+    return addresses
+
+def delete_address(row_id: int) -> None:
+    """Удаляет адрес по его идентификатору"""
+    db.delete("addresses", row_id)
--- server.py
+++ server.py
@@ -6,11 +6,10 @@
 # https://github.com/alexey-goloburdin/telegram-finance-bot
 
 """Сервер Telegram бота, запускаемый непосредственно"""
-
+import configparser
 import logging
 import os
 
-import aiohttp
 from aiogram import Bot, Dispatcher, executor, types
 
 import exceptions
@@ -19,6 +18,7 @@ import exceptions
 #from middlewares import AccessMiddleware
 
 from yandex import YandexMap
+import addresses
 
 
 
@@ -32,6 +32,20 @@ YANDEX_TOKEN = os.getenv("YANDEX_API_KEY")
 #    password=os.getenv("TELEGRAM_PROXY_PASSWORD")
 #)
 
+# Если Windows - то пытаемся читать из файла окружения
+if API_TOKEN == None:
+    tokens = configparser.ConfigParser()
+    tokens.read('.env')
+    API_TOKEN = tokens.get('KEYS', 'TELEGRAM_API_TOKEN')
+
+if YANDEX_TOKEN == None:
+    tokens = configparser.ConfigParser()
+    tokens.read('.env')
+    YANDEX_TOKEN = tokens.get('KEYS', 'YANDEX_API_KEY')
+
+#print('TELEGRAM_API_TOKEN', API_TOKEN)
+#print('YANDEX_API_TOKEN', YANDEX_TOKEN)
+
 
 bot = Bot(token=API_TOKEN, proxy=PROXY_URL)
 dp = Dispatcher(bot)
@@ -41,12 +55,32 @@ async def send_welcome(message: types.Message):
     """Отправляет приветственное сообщение и помощь по боту"""
     await message.answer(
         "Бот для преобразования адреса в ссылку, ведущую на Яндекс.Карты, где находится этот адрес\n\n"
-        "Добавить адрес: Нахимовский проспект 1\n"
-        #"Все мои адреса: /adresses\n"
+        "Добавить адрес: Нахимовский проспект 1\n\n"
+        "Все мои адреса: /addresses\n\n"
         #"Последние адреса: /last_adresses\n"
         #"Категории трат: /categories"
+        "Автор: Сальдиков Иван (c) 2020\n"
+        "saldoz@ya.ru"
     )
 
+
+@dp.message_handler(commands=['addresses'])
+async def show_user_addresses(message: types.Message):
+    """Отправляет список всех адресов пользователя"""
+    all_addresses = addresses.get_all_addresses(message.from_user.id)
+    if not all_addresses:
+        await message.answer("Вы ещё не сохранили ни одного адреса")
+        return
+
+    addresses_rows = [
+        f"*{address_one.address}* - /goto{address_one.id}\n"
+        f"/addtocat - добавить в категорию\n"
+        f"/del{address_one.id} - удалить адрес"
+        for address_one in all_addresses]
+    answer_message = "Ваш список адресов:\n\n" + "\n\n"\
+            .join(addresses_rows) + \
+            "\n\nНачальный экран: /start"
+    await message.answer(answer_message, parse_mode= "Markdown")
 
 @dp.message_handler()
 async def add_address(message: types.Message):
@@ -63,14 +97,30 @@ async def add_address(message: types.Message):
                 long = arr[0]
                 wide = arr[1]
                 link_to_yamaps = yandex_map.form_href_to_yamap(long, wide)
+        db_id = addresses.add_address(message.text,
+                                      link_to_yamaps,
+                                      message.from_user.id)
     except exceptions.NotCorrectMessage as e:
         await message.answer(str(e))
         return
     answer_message = (
-        f"Ваша ссылка: {link_to_yamaps}.\n\n"
+        f"Ваша ссылка на Яндекс.Карты: {link_to_yamaps}.\n\n"
+        f"Все Ваши адреса: /addresses\n\n"
+        f"Главное меню: /start\n\n"
         )
     await message.answer(answer_message)
 
+@dp.message_handler(lambda message: message.text.startswith('/del'))
+async def del_expense(message: types.Message):
+    """Удаляет одну запись об адресе по её идентификатору"""
+    row_id = int(message.text[4:])
+    addresses.delete_address(row_id)
+    answer_message = (
+        f"Все Ваши адреса: /addresses\n\n"
+        f"Главное меню: /start\n\n"
+        )
+    await message.answer(answer_message)
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
+
